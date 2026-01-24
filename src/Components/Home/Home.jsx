603388997c02aa15345f02a5e5ceb5b9a5288f2c
Rihan Dhakal <rihandhakal10@gmail.com>
2026-01-24 16:57:35 +0545

    fixed bug

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../navbar/Navbar'
+// import Navbar from '../Navbar/Navbar'
 import Hero from '../Hero/Hero'
 // import Button from '../Button/Button'
 import Category from '../Category/Category'
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Navbar from '../navbar/Navbar'
+
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer/Footer'
+import Navbar from '../Navbar/Navbar'
 
 export const Layout = () => {
     return (
